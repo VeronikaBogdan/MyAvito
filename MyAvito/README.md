1b27e7f2dd17159ee75b70c1cfe466dfbfa675be
VeronikaBogdan <84421935+VeronikaBogdan@users.noreply.github.com>
2021-07-30 18:41:45 +0300

    Slider

---
+++ MyAvito/README.md
@@ -0,0 +1,12 @@
+# MyAvito -- сайт объявлений
+Учебный проект Glo Academy
+
+## Выполнила проект -- Богдан Вероника
+
+### Технологии в проекте
+- HTML
+- CSS
+- JavaScript
+- Normalize
+- FancyBox
+- 
---
+++ js/main.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters -- Параметры слайдера
+    //direction: 'vertical',
+    loop: true, // бесконечная прокрутка
+  
+    // If we need pagination -- если нужна навигация
+    /*pagination: {
+      el: '.swiper-pagination',
+    },*/
+  
+    // Navigation arrows -- стрелки
+    /*navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },*/
+  
+    // And if we need scrollbar -- Лента прокрутки
+    /*scrollbar: {
+      el: '.swiper-scrollbar',
+    },*/
+  });
