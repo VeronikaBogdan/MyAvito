580f080b0022d7ad451452fc52648690731a1726
VeronikaBogdan <84421935+VeronikaBogdan@users.noreply.github.com>
2021-07-30 19:49:37 +0300

    Finish ad page (announcement)

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters -- Параметры слайдера
-    //direction: 'vertical',
-    loop: true, // бесконечная прокрутка
-  
-    // If we need pagination -- если нужна навигация
-    /*pagination: {
-      el: '.swiper-pagination',
-    },*/
-  
-    // Navigation arrows -- стрелки
-    /*navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },*/
-  
-    // And if we need scrollbar -- Лента прокрутки
-    /*scrollbar: {
-      el: '.swiper-scrollbar',
-    },*/
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
   });
